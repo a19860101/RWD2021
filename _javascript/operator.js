533f844359ad48f4494548990d9485e1e7258380
Zac <a19860101@gmail.com>
2021-12-12 11:15:34 +0800

    20211212

--- _javascript/operator.js
+++ _javascript/operator.js
@@ -19,7 +19,7 @@ console.log(x == y);
 console.log(x === y);//值與資料型態皆須相同
 console.log(x != y);
 
-let a = 10;
+let a = 'asdf';
 let b = '10';
 console.log(a == b);
 console.log(a === b);
@@ -32,3 +32,14 @@ console.log(x *= y);
 console.log(x /= y);
 console.log(x %= y);
 console.log(x);
+
+// 邏輯運算子
+// AND OR NOT
+// && || !
+
+// 三元運算子
+
+// let result = a > 0 ? 'success':'error';
+let result = a > 0 ? '正數':a < 0 ? '負數':'error';
+
+console.log(result);
