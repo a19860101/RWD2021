0caa9737d24b1741179e6141405b4093fd4ec7cc
Zac <a19860101@gmail.com>
2021-12-12 11:03:38 +0800

    20211212

---
+++ _javascript/operator.js
@@ -0,0 +1,34 @@
+// 運算子
+
+// 算術運算子
+let x = 10;
+let y = 6;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+// 比較運算子
+console.log(x > y);
+console.log(x >= y);
+console.log(x < y);
+console.log(x <= y);
+console.log(x == y);
+console.log(x === y);//值與資料型態皆須相同
+console.log(x != y);
+
+let a = 10;
+let b = '10';
+console.log(a == b);
+console.log(a === b);
+
+// 指定運算子
+console.log(x = y);
+console.log(x += y); // x = x + y
+console.log(x -= y);
+console.log(x *= y);
+console.log(x /= y);
+console.log(x %= y);
+console.log(x);
