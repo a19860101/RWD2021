30554d85351f203aa3fac35118fe4cdfaca82a3d
Zac <a19860101@gmail.com>
2021-12-12 10:16:27 +0800

    20211212

---
+++ _javascript/script.js
@@ -0,0 +1,37 @@
+// 變數 variable
+var a = 100;
+
+var a = 'hello';
+
+let b = 999;
+// let b = 'JAVASCRIPT';
+
+console.log(b);
+
+/* 
+變數命名規範
+1.只能用英文底線錢字號開頭
+2.大小寫有別
+3.不可使用特殊符號除了底線跟前字號
+4.不可使用保留字
+*/
+
+/* 
+    var vs let ,const
+    
+    var 可重複宣告 let,const 不可重複宣告
+    範疇不同
+
+*/
+
+//常數 constant variable
+// 不可重新賦值
+// 通常使用在物件陣列函式
+
+const x = 10;
+
+// x = x + 1;
+
+const ary = ['apple'];
+// ary = ['apple','banana'];
+// ary.push('banana');
