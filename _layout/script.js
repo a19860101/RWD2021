2e280c09c72d567aee737c58cb2eae686ece173b
Zac <a19860101@gmail.com>
2022-02-13 14:10:21 +0800

    20220213

--- _layout/script.js
+++ _layout/script.js
@@ -5,10 +5,32 @@ $(function(){
         $('html,body').animate({
             scrollTop: distance
         })
+        return false;
     })
     $('.gotop').click(function(){
         $('html,body').animate({
             scrollTop: 0
+        })
+        return false;
+    })
+    $(window).scroll(function(){
+        let h = $(window).scrollTop();
+        console.log(h);
+        let _in = 'bounceIn';
+        let _out = 'hinge';
+        if(h > 650){
+            $('.gotop').show().removeClass('animated '+_out).addClass('animated '+_in);
+        }else{
+            $('.gotop').removeClass('animated '+_in).addClass('animated '+_out);
+        }
+        $('.scroll').each(function(){
+            let scroll = $(this).data('scroll');
+            let o = $(scroll).offset().top;
+            console.log(o)
+            if(h >= o - 200){
+                $('.scroll').removeClass('active');
+                $(this).addClass('active');
+            }
         })
     })
 })
